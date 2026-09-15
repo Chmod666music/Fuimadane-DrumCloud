deac46631ce31aac8c3465a10b765bdce87202b3
Chmod 666 <engleboef@gmail.com>
2026-09-15 22:48:52 +0200

    Define streaming waveform preview result

--- AudioFileLoader.hpp
+++ AudioFileLoader.hpp
@@ -12,3 +12,16 @@ struct LoadedAudio
 };
 
 bool loadAudioFileToFloat(const char* path, LoadedAudio& out, std::string* err = nullptr);
+
+struct AudioPreview
+{
+    static constexpr uint32_t kBins = 1024;
+    float min[kBins]{};
+    float max[kBins]{};
+    uint32_t channels = 0;
+    uint32_t sampleRate = 0;
+    uint64_t frames = 0;
+};
+
+// Streams only a small decoder block at a time; does not retain the PCM.
+bool loadAudioFilePreview(const char* path, AudioPreview& out, std::string* err = nullptr);
