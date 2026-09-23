de55c41e5be9613e389658534816b88320059afd
Chmod 666 <engleboef@gmail.com>
2026-09-23 19:28:05 +0200

    Add polyphonic MIDI slicer mode

--- tools/linux-regression.sh
+++ tools/linux-regression.sh
@@ -41,6 +41,10 @@ g++ -std=gnu++17 -O2 -Wall -Wextra tests/time_stretch.cpp -I. \
     -o "$reg_tmp/time-stretch-test"
 "$reg_tmp/time-stretch-test"
 
+g++ -std=gnu++17 -O2 -Wall -Wextra tests/slice_mapping.cpp -I. \
+    -o "$reg_tmp/slice-mapping-test"
+"$reg_tmp/slice-mapping-test"
+
 echo "[2/4] Building Linux CLAP and VST3"
 ./build.sh
 
